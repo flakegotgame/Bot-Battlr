59fa98e9b67fdc93e6b1c77950b68cab7c0c97d8
flakegotgame <victorchange808@gmail.com>
2024-05-06 10:11:59 +0300

    first commit

---
+++ bot-battlr/components/botCollection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BotCard from './BotCard';
+import { Link } from 'react-router-dom';
+
+function Botcollection({ bots }) {
+  const botCards = bots.map(bot => (
+    <Link to={`/bots/${bot.id}`} key={bot.id}>
+      <BotCard bot={bot} />
+    </Link>
+  ));
+
+  return (
+    <div className="bot-collection">
+      {botCards}
+    </div>
+  );
+}
+
+export default Botcollection;
